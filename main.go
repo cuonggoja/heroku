c2fada1e4ec051c599d1c65b4e12383042ec9ef6
manhcuong <manhcuongjavaweb@gmail.com>
2020-12-07 16:26:04 +0700

    test deloy heroku

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", HelloServer)
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Fatalf("Error creating server %s\n", err.Error())
+	}
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
